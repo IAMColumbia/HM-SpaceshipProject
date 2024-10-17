b458172047248731ed1b76de1c62071224634fd6
bob-jone <hdm210@gmail.com>
2024-10-17 11:52:45 -0500

    Add player movement and max speed constraint

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -5,17 +5,38 @@ using UnityEngine;
 public class SpaceshipController : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody _rb;
+    private float _playerSpeed;
+    [SerializeField]
+    private float _maxSpeed;
 
+    private Rigidbody _rb;
+    private float _horizontalInput;
+    private float _verticalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(_horizontalInput,0,_verticalInput);
+
+        _rb.AddForce(movementDirection * _playerSpeed * Time.deltaTime);
+
+        LimitShipSpeed(movementDirection);
+    }
+
+    // limit the speed of the ship to the maximum speed
+    void LimitShipSpeed(Vector3 moveDirection)
+    {
+        if (_rb.velocity.magnitude > _maxSpeed)
+        {
+            _rb.velocity = moveDirection * _maxSpeed;
+        }
     }
 }
